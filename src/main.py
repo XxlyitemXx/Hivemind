422e4a95029c18fcf3d3da5e644bbef308b3b1ad
XxlyitemXx <temjaitnt@gmail.com>
2024-11-24 17:38:52 +0700

    minor update

--- src/main.py
+++ src/main.py
@@ -1,23 +1,37 @@
 import nextcord
 from nextcord.ext import commands
 from assets.utils.config_loader import load_config
-import os
-from dotenv import load_dotenv
 from assets.utils.logger import Logger
+import requests
+
 logger = Logger()
-# Load environment variables
-load_dotenv()
 
 config = load_config()
 intents = nextcord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user}')
+    print(f"Logged in as {bot.user}")
+    try:
+        public_ip = requests.get("https://api.ipify.org").text
+        await bot.change_presence(
+            activity=nextcord.Activity(
+                type=nextcord.ActivityType.watching, name=public_ip
+            )
+        )
+    except:
+        logger.error("Could not fetch public IP address", "STARTUP")
+        await bot.change_presence(
+            activity=nextcord.Activity(
+                type=nextcord.ActivityType.watching, name="IP Unavailable"
+            )
+        )
     logger.info(f"Bot logged in as {bot.user}", "STARTUP")
+
 
 bot.load_extension("command.ls")
 bot.load_extension("command.pull")
@@ -25,8 +39,6 @@ bot.load_extension("command.system_info")
 bot.load_extension("command.terminal")
 bot.load_extension("command.register")
 bot.load_extension("command.logs")
-
-
 
 
 if __name__ == "__main__":
